0ae4c675d059748ccb533090b655aa1c66045437
Fran <frangalo34@gmail.com>
2023-11-04 11:14:57 +0100

    chage navBar in order to have burger menu when the screen is under 1024 px

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,55 +1,39 @@
+"use client"
+import React, { useState, useEffect } from "react";
 import {
 	Navbar as NextUINavbar,
 	NavbarContent,
-	NavbarMenu,
-	NavbarMenuToggle,
 	NavbarBrand,
 	NavbarItem,
+	NavbarMenu,
 	NavbarMenuItem,
+	NavbarMenuToggle,
 } from "@nextui-org/navbar";
-import { Button } from "@nextui-org/button";
-import { Kbd } from "@nextui-org/kbd";
-import { Link } from "@nextui-org/link";
-import { Input } from "@nextui-org/input";
-
-import { link as linkStyles } from "@nextui-org/theme";
-
 import { siteConfig } from "@/config/site";
+import { Link } from "@nextui-org/link";
 import NextLink from "next/link";
+import { button as buttonStyles } from "@nextui-org/theme";
+import { link as linkStyles } from "@nextui-org/theme";
 import clsx from "clsx";
-
-import { ThemeSwitch } from "@/components/theme-switch";
-import {
-	TwitterIcon,
-	GithubIcon,
-	DiscordIcon,
-	HeartFilledIcon,
-	SearchIcon,
-} from "@/components/icons";
-
+import { GithubIcon } from "@/components/icons";
 import { Logo } from "@/components/icons";
 
 export const Navbar = () => {
-	const searchInput = (
-		<Input
-			aria-label="Search"
-			classNames={{
-				inputWrapper: "bg-default-100",
-				input: "text-sm",
-			}}
-			endContent={
-				<Kbd className="hidden lg:inline-block" keys={["command"]}>
-					K
-				</Kbd>
-			}
-			labelPlacement="outside"
-			placeholder="Search..."
-			startContent={
-				<SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-			}
-			type="search"
-		/>
-	);
+	const [isSmallScreen, setIsSmallScreen] = useState(false);
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+	useEffect(() => {
+		const handleResize = () => {
+			setIsSmallScreen(window.innerWidth <= 1024);
+		};
+
+		window.addEventListener("resize", handleResize);
+		handleResize();
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
 
 	return (
 		<NextUINavbar maxWidth="xl" position="sticky">
@@ -57,25 +41,51 @@ export const Navbar = () => {
 				<NavbarBrand as="li" className="gap-3 max-w-fit">
 					<NextLink className="flex justify-start items-center gap-1" href="/">
 						<Logo />
-						<p className="font-bold text-inherit">ACME</p>
 					</NextLink>
 				</NavbarBrand>
-				<ul className="hidden lg:flex gap-4 justify-start ml-2">
-					{siteConfig.navItems.map((item) => (
-						<NavbarItem key={item.href}>
-							<NextLink
-								className={clsx(
-									linkStyles({ color: "foreground" }),
-									"data-[active=true]:text-primary data-[active=true]:font-medium"
-								)}
-								color="foreground"
-								href={item.href}
-							>
-								{item.label}
-							</NextLink>
-						</NavbarItem>
-					))}
-				</ul>
+				{isSmallScreen ? (
+					<>
+						<NavbarMenuToggle
+							aria-label={isMobileMenuOpen ? "Close menu" : "Open menu"}
+							className="lg:hidden"
+							onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+						/>
+						{isMobileMenuOpen && (
+							<NavbarMenu>
+								{siteConfig.navItems.map((item) => (
+									<NavbarMenuItem key={item.href}>
+										<Link
+											className={clsx(
+												linkStyles({ color: "foreground" }),
+												"data-[active=true]:text-primary data-[active=true]:font-medium"
+											)}
+											href={item.href}
+										>
+											{item.label}
+										</Link>
+									</NavbarMenuItem>
+								))}
+							</NavbarMenu>
+						)}
+					</>
+				) : (
+					<ul className="hidden lg:flex gap-4 justify-start ml-2">
+						{siteConfig.navItems.map((item) => (
+							<NavbarItem key={item.href}>
+								<NextLink
+									className={clsx(
+										linkStyles({ color: "foreground" }),
+										"data-[active=true]:text-primary data-[active=true]:font-medium"
+									)}
+									color="foreground"
+									href={item.href}
+								>
+									{item.label}
+								</NextLink>
+							</NavbarItem>
+						))}
+					</ul>
+				)}
 			</NavbarContent>
 
 			<NavbarContent
@@ -83,62 +93,33 @@ export const Navbar = () => {
 				justify="end"
 			>
 				<NavbarItem className="hidden sm:flex gap-2">
-					<Link isExternal href={siteConfig.links.twitter} aria-label="Twitter">
-						<TwitterIcon className="text-default-500" />
-					</Link>
-					<Link isExternal href={siteConfig.links.discord} aria-label="Discord">
-						<DiscordIcon className="text-default-500" />
-					</Link>
 					<Link isExternal href={siteConfig.links.github} aria-label="Github">
 						<GithubIcon className="text-default-500" />
 					</Link>
-					<ThemeSwitch />
-				</NavbarItem>
-				<NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem>
-				<NavbarItem className="hidden md:flex">
-					<Button
-            isExternal
-						as={Link}
-						className="text-sm font-normal text-default-600 bg-default-100"
-						href={siteConfig.links.sponsor}
-						startContent={<HeartFilledIcon className="text-danger" />}
-						variant="flat"
-					>
-						Sponsor
-					</Button>
 				</NavbarItem>
 			</NavbarContent>
 
-			<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-				<Link isExternal href={siteConfig.links.github} aria-label="Github">
-					<GithubIcon className="text-default-500" />
-				</Link>
-				<ThemeSwitch />
-				<NavbarMenuToggle />
-			</NavbarContent>
+			{!isSmallScreen && (
+				<NavbarContent>
 
-			<NavbarMenu>
-				{searchInput}
-				<div className="mx-4 mt-2 flex flex-col gap-2">
-					{siteConfig.navMenuItems.map((item, index) => (
-						<NavbarMenuItem key={`${item}-${index}`}>
-							<Link
-								color={
-									index === 2
-										? "primary"
-										: index === siteConfig.navMenuItems.length - 1
-										? "danger"
-										: "foreground"
-								}
-								href="#"
-								size="lg"
-							>
-								{item.label}
-							</Link>
-						</NavbarMenuItem>
-					))}
-				</div>
-			</NavbarMenu>
+				</NavbarContent>
+			)}
+
+			{isSmallScreen && (
+				<NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
+					<div className="flex gap-3">
+						<Link
+							isExternal
+							as={NextLink}
+							className={buttonStyles({ variant: "bordered", radius: "full" })}
+							href={siteConfig.links.github}
+						>
+							<GithubIcon size={20} />
+							GitHub
+						</Link>
+					</div>
+				</NavbarContent>
+			)}
 		</NextUINavbar>
 	);
 };
